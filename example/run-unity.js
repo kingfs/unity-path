5f906c75ef8708e90140508cac844a3229cc6d8b
Kyle Van Berendonck <kvanberendonck@gmail.com>
2017-10-09 19:12:57 +1100

    Test new travis script ..

--- example/run-unity.js
+++ example/run-unity.js
@@ -1,3 +1,3 @@
 'use strict'
 var unityPath = require('../dist/index.js').unityPath
-console.log('Unity is located at:', unityPath())
+unityPath().then(function(path) { console.log('Unity is located at:', path); })
