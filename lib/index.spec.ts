5343c1465248ddbd6969c7dea2710355d0e940fb
Kyle Van Berendonck <kvanberendonck@gmail.com>
2017-10-09 16:30:40 +1100

    Pass all lints, pass all tests

--- lib/index.spec.ts
+++ lib/index.spec.ts
@@ -1,28 +1,34 @@
 import * as fs from 'fs';
+import * as path from 'path';
+import * as temp from 'temp';
 import { unityPath } from './index';
 
-import * as mocha from 'mocha';
 import * as chai from 'chai';
 import * as chaiAsPromised from 'chai-as-promised';
+import * as mocha from 'mocha';
 import * as sinon from 'sinon';
 import * as sinonChai from 'sinon-chai';
 
+temp.track()
 chai.use(sinonChai);
 chai.use(chaiAsPromised);
 const expect = chai.expect;
 
 const unityInstallPaths = {
-  'debian':   '/opt/Unity/Editor/Unity',
-  'osx':      '/Applications/Unity/Unity.app/Contents/MacOS/Unity',
-  'win':      'C:\\Program Files\\Unity\\Editor\\Unity.exe',
-  'wow64':    'C:\\Program Files (x86)\\Unity\\Editor\\Unity.exe'
+  debian:   '/opt/Unity/Editor/Unity',
+  osx:      '/Applications/Unity/Unity.app/Contents/MacOS/Unity',
+  win:      'C:\\Program Files\\Unity\\Editor\\Unity.exe',
+  wow64:    'C:\\Program Files (x86)\\Unity\\Editor\\Unity.exe',
 }
 
 describe('unity-path', () => {
-  it('should export a function', () => {
+  beforeEach(() => {
+    delete process.env.UNITY_PATH /* unset */
+  })
+  it('should export the function unityPath', () => {
     expect(unityPath).to.be.a('function')
   })
-  describe('unityPath()', () => {
+  describe('unityPath(): stubbed', () => {
     let access: sinon.SinonStub;
     beforeEach(() => {
       access = sinon.stub(fs, 'access')
@@ -33,10 +39,10 @@ describe('unity-path', () => {
     })
     it('should try default paths in order', () => {
       // all stubs throw not found error
-      let debianStub = sinon.stub().callsArgWithAsync(2, new Error('<error>'))
-      let osxStub = sinon.stub().callsArgWithAsync(2, new Error('<error>'))
-      let winStub = sinon.stub().callsArgWithAsync(2, new Error('<error>'))
-      let wow64Stub = sinon.stub().callsArgWithAsync(2, new Error('<error>'))
+      const debianStub = sinon.stub().callsArgWithAsync(2, new Error('<error>'))
+      const osxStub = sinon.stub().callsArgWithAsync(2, new Error('<error>'))
+      const winStub = sinon.stub().callsArgWithAsync(2, new Error('<error>'))
+      const wow64Stub = sinon.stub().callsArgWithAsync(2, new Error('<error>'))
       // wire up stubs
       access.withArgs(unityInstallPaths.debian).callsFake(debianStub)
       access.withArgs(unityInstallPaths.osx).callsFake(osxStub)
@@ -52,29 +58,165 @@ describe('unity-path', () => {
           expect(wow64Stub).to.have.been.calledOnce.and.calledAfter(winStub)
         })
     })
-    it('should find Unity at default path on Debian/Ubuntu', () => {
-      let debianStub = sinon.stub().callsArgAsync(2)
+    it('should resolve Unity at default path on Debian/Ubuntu', () => {
+      const debianStub = sinon.stub().callsArgAsync(2)
       access.withArgs(unityInstallPaths.debian).callsFake(debianStub)
       return expect(unityPath()).to.eventually.equal(unityInstallPaths.debian)
     })
-    it('should find Unity at default path on OS X', () => {
-      let osxStub = sinon.stub().callsArgAsync(2)
+    it('should resolve Unity at default path on OS X', () => {
+      const osxStub = sinon.stub().callsArgAsync(2)
       access.withArgs(unityInstallPaths.osx).callsFake(osxStub)
       return expect(unityPath()).to.eventually.equal(unityInstallPaths.osx)
     })
-    it('should find Unity at default path on Windows', () => {
-      let winStub = sinon.stub().callsArgAsync(2)
+    it('should resolve Unity at default path on Windows', () => {
+      const winStub = sinon.stub().callsArgAsync(2)
       access.withArgs(unityInstallPaths.win).callsFake(winStub)
       return expect(unityPath()).to.eventually.equal(unityInstallPaths.win)
     })
-    it('should find Unity at default path on Windows/WOW64', () => {
-      let wow64Stub = sinon.stub().callsArgAsync(2)
+    it('should resolve Unity at default path on Windows/WOW64', () => {
+      const wow64Stub = sinon.stub().callsArgAsync(2)
       access.withArgs(unityInstallPaths.wow64).callsFake(wow64Stub)
       return expect(unityPath()).to.eventually.equal(unityInstallPaths.wow64)
     })
     it('should reject if Unity is not found at any of the default paths', () => {
       expect(unityPath()).to.be.rejectedWith(
-        /^Unable to locate Unity installation, tried all of these paths:( "([^"]{2,})"[,\.]){4} Try setting env \'UNITY_PATH\' or supplying a path to check as first argument\.$/)
+        // tslint:disable-next-line:max-line-length
+        /^Unable to locate Unity installation, tried all of these paths:( "([^"]{10,})"[,\.]){4} Try setting env \'UNITY_PATH\' or supplying a path to check as first argument\.$/)
+    })
+    it('should check env UNITY_PATH rather than the default search paths if it\'s valid', () => {
+      process.env.UNITY_PATH = path.resolve(temp.mkdirSync('unity-path-tests'), 'Unity')
+      return expect(unityPath()).to.eventually.equal(process.env.UNITY_PATH)
+    })
+    it('should still access the default search locations if env UNITY_PATH is set without a value', () => {
+      process.env.UNITY_PATH = ''
+      return expect(unityPath()).to.be.rejected;
+    })
+  })
+  describe('unityPath(path: string)', () => {
+    it('should throw an exception if given a path that is of invalid type', () => {
+      return expect(unityPath({} as any))
+        .to.be.rejectedWith(
+          // tslint:disable-next-line:max-line-length
+          /^Could not understand input as a valid argument: "[^"]{5,}"\. A valid argument is either a path \(or series of paths\) to try\.$/,
+        )
+    })
+    it('should check the provided path rather than the default search paths', () => {
+      const tmpDir = temp.mkdirSync('unity-path-tests')
+      const binPath = path.resolve(tmpDir, 'Unity')
+      fs.writeFileSync(binPath, '#/bin/sh\nexit 0')
+      return expect(unityPath(binPath))
+        .to.be.fulfilled
+        .then((result) => {
+          expect(result).to.be.a('string').that.is.not.null
+          expect(result).to.equal(binPath)
+        })
+    })
+    it('should save the returned path as env UNITY_PATH so that repeated invocations re-use it', () => {
+      const tmpDir = temp.mkdirSync('unity-path-tests')
+      const binPath = path.resolve(tmpDir, 'Unity')
+      fs.writeFileSync(binPath, '#/bin/sh\nexit 0')
+      return expect(unityPath(binPath))
+        .to.be.fulfilled
+        .then((result) => {
+          expect(process.env.UNITY_PATH).to.equal(binPath)
+        })
+    })
+    it('should be possible to manually overwrite the path that has been resolved', () => {
+      const tmpDir = temp.mkdirSync('unity-path-tests')
+      const binPath1 = path.resolve(tmpDir, 'Unity1')
+      const binPath2 = path.resolve(tmpDir, 'Unity2')
+      fs.writeFileSync(binPath1, '#/bin/sh\nexit 0')
+      fs.writeFileSync(binPath2, '#/bin/sh\nexit 0')
+      // invoke with specific path to first Unity binary
+      return expect(unityPath(binPath1))
+        .to.be.fulfilled
+        .then((first) => {
+          expect(first).to.be.a('string').that.is.not.null
+          expect(process.env.UNITY_PATH).to.equal(binPath1)
+          expect(first).to.equal(binPath1)
+          // invoke with specific path to second Unity binary, check it is overriden
+          return expect(unityPath(binPath2))
+            .to.be.fulfilled
+            .then((second) => {
+              expect(second).to.be.a('string').that.is.not.null
+              expect(second).not.to.be.equal(first)
+              expect(second).to.equal(binPath2)
+            })
+        })
+    })
+    it('should trim a path that is padded with spaces', () => {
+      const tmpDir = temp.mkdirSync('unity-path-tests')
+      const binPath = path.resolve(tmpDir, 'Unity')
+      const paddedBinPath = '   ' + binPath + '   '
+      fs.writeFileSync(binPath, '#/bin/sh\nexit 0')
+      return expect(unityPath(paddedBinPath))
+        .to.be.fulfilled
+        .then((result) => {
+          expect(result).to.be.a('string').that.is.not.null
+          expect(process.env.UNITY_PATH).to.equal(binPath)
+          expect(result).not.to.equal(paddedBinPath)
+          expect(result).to.equal(binPath)
+        })
+    })
+    it('should trim env UNITY_PATH if it has been padded with spaces', () => {
+      const tmpDir = temp.mkdirSync('unity-path-tests')
+      const binPath = path.resolve(tmpDir, 'Unity')
+      const paddedBinPath = '   ' + binPath + '   '
+      fs.writeFileSync(binPath, '#/bin/sh\nexit 0')
+      process.env.UNITY_PATH = paddedBinPath
+      return expect(unityPath())
+        .to.be.fulfilled
+        .then((result) => {
+          expect(result).to.be.a('string').that.is.not.null
+          expect(process.env.UNITY_PATH).to.equal(binPath)
+          expect(result).not.to.equal(paddedBinPath)
+          expect(result).to.equal(binPath)
+        })
+    })
+  })
+  describe('unityPath(paths: string[])', () => {
+    let access: sinon.SinonStub;
+    beforeEach(() => {
+      access = sinon.stub(fs, 'access')
+      access.callsArgWithAsync(2, new Error('<error>'))
+    })
+    afterEach(() => {
+      access.restore();
+    })
+    it('should check the provided array of paths rather than the default search paths', () => {
+      const tmpDir = temp.mkdirSync('unity-path-tests')
+      const fakePathToUnity = path.resolve(tmpDir, 'Unity_fake')
+      const myPaths = [unityInstallPaths.win, unityInstallPaths.wow64, fakePathToUnity]
+      // all stubs throw not found error
+      const debianStub = sinon.stub().callsArgWithAsync(2, new Error('<error>'))
+      const osxStub = sinon.stub().callsArgWithAsync(2, new Error('<error>'))
+      const winStub = sinon.stub().callsArgWithAsync(2, new Error('<error>'))
+      const wow64Stub = sinon.stub().callsArgWithAsync(2, new Error('<error>'))
+      const fakePathToUnityStub = sinon.stub().callsArgWithAsync(2, new Error('<error>'))
+      // wire up stubs
+      access.withArgs(unityInstallPaths.debian).callsFake(debianStub)
+      access.withArgs(unityInstallPaths.osx).callsFake(osxStub)
+      access.withArgs(unityInstallPaths.win).callsFake(winStub)
+      access.withArgs(unityInstallPaths.wow64).callsFake(wow64Stub)
+      access.withArgs(fakePathToUnity).callsFake(fakePathToUnityStub)
+      return expect(unityPath(myPaths))
+        .to.be.rejected
+        .then(() => {
+          expect(access)
+          expect(winStub).to.be.calledOnce
+          expect(wow64Stub).to.be.calledOnce
+          expect(fakePathToUnityStub).to.be.calledOnce
+          expect(debianStub).not.to.be.called
+          expect(osxStub).not.to.be.called
+        })
+    })
+    it('should behave the same as unityPath() if an empty list is passed', () => {
+      const myPaths = []
+      return expect(unityPath(myPaths))
+        .to.be.rejected
+        .then(() => {
+          expect(access.callCount).to.equal(4)
+        })
     })
   })
 });
--- lib/index.ts
+++ lib/index.ts
@@ -1,27 +1,27 @@
-import * as process from 'process'
 import * as fs from 'fs'
-import * as pify from 'pify'
 import { isString } from 'lodash'
+import * as pify from 'pify'
+import * as process from 'process'
 
 const unityInstallPaths = [
   '/opt/Unity/Editor/Unity',
   '/Applications/Unity/Unity.app/Contents/MacOS/Unity',
   'C:\\Program Files\\Unity\\Editor\\Unity.exe',
-  'C:\\Program Files (x86)\\Unity\\Editor\\Unity.exe'
+  'C:\\Program Files (x86)\\Unity\\Editor\\Unity.exe',
 ]
-
-let resolvedUnityPath: string = null
 
 export function unityPath(path?: string|string[]): Promise<string> {
   // if no argument is given, act as a getter.
-  if (path === undefined) {
-    // if the resolved path is valid, return it immediately
-    if (isString(resolvedUnityPath)) {
-      return Promise.resolve(resolvedUnityPath)
-    }
-    // otherwise check for the UNITY_PATH environment variable
-    else if (isString(process.env.UNITY_PATH)) {
-      return unityPath(process.env.UNITY_PATH)
+  if (path === undefined || path === null) {
+    // if the sessions' saved path is valid, return it immediately
+    if (isString(process.env.UNITY_PATH) && process.env.UNITY_PATH.length >= 1) {
+      // clean up the path
+      const trimmedPath = process.env.UNITY_PATH.trim()
+      if (trimmedPath !== process.env.UNITY_PATH) {
+        return unityPath(trimmedPath)
+      } else {
+        return Promise.resolve(process.env.UNITY_PATH)
+      }
     }
     // if not present, use defaults
     else {
@@ -32,26 +32,31 @@ export function unityPath(path?: string|string[]): Promise<string> {
   // if the provided string is invalid, on re-invocation defaults will be restored.
   else if (isString(path)) {
     path = path as string
-    resolvedUnityPath = path
+    process.env.UNITY_PATH = path.trim()
     return unityPath()
   }
   // if the argument is an array, try the paths specified.
   // the provided order of the paths determines their priority when checking if the executable exists.
   else if (Array.isArray(path)) {
-    let paths = path as Array<string>
-    if (paths.length <= 0) return unityPath()
+    const paths = path as string[]
+    if (paths.length <= 0) { return unityPath() }
 
     // based on mitmadness/UnityInvoker
     return paths.reduce((acc, curr) =>
       acc.then(
-        val => Promise.resolve(val),
-        err => pify(fs.access)(curr, fs.constants.X_OK)
-                 .then(_ => Promise.resolve(curr), _ => Promise.reject(err))
+        (val) => Promise.resolve(val),
+        (err) => pify(fs.access)(curr, fs.constants.X_OK)
+                 .then((_) => Promise.resolve(curr), (_) => Promise.reject(err)),
       ),
       Promise.reject(
         `Unable to locate Unity installation, tried all of these paths: ` +
-        `${paths.map(p => `"${p}"`).join(', ')}. ` +
-        `Try setting env 'UNITY_PATH' or supplying a path to check as first argument.`
+        `${paths.map((p) => `"${p}"`).join(', ')}. ` +
+        `Try setting env 'UNITY_PATH' or supplying a path to check as first argument.`,
       ))
+  } else {
+    return Promise.reject(
+      `Could not understand input as a valid argument: "${path}". ` +
+      `A valid argument is either a path (or series of paths) to try.`,
+    )
   }
 }
